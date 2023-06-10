140af7873cc16d9f7950ba7826c1250842b4d352
ramon <raseniero@gmail.com>
2023-06-10 02:05:26 -0700

    added new fields to patient

--- patient/models.py
+++ patient/models.py
@@ -4,8 +4,20 @@ from django.db import models
 # Create your models here.
 class Patient(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=True, default="")
-    age = models.IntegerField()
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    birth_place = models.CharField(max_length=100, blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)
+    telephone_number = models.CharField(max_length=100, blank=True, null=True)
+    image = models.ImageField(upload_to="images/", blank=True, null=True)
 
     class Meta:
         db_table = "patient"
