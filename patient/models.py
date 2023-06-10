d49ba76789706e7be4a9f03fb0048c6de4b7fee6
ramon <raseniero@gmail.com>
2023-06-09 21:26:43 -0700

    added init patient model

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=True, default="")
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = "patient"
+        verbose_name = "Patient"
+        verbose_name_plural = "Patients"
+
+    def __str__(self):
+        return self.name
