2491383429a0dc175e638f6c836576aa921eaa60
ramon <raseniero@gmail.com>
2023-06-09 22:13:04 -0700

    added patient url endpoints

---
+++ patient/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path(
+        "patients/", views.PatientListCreate.as_view(), name="patient-list"
+    ),  # patient list view
+    path(
+        "patients/<int:pk>/",
+        views.PatientRetrieveUpdateDestroy.as_view(),
+        name="patient-detail",
+    ),  # patient detail view
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
